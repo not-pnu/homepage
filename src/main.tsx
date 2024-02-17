e084ca465bb0a739d8157a8ea1a9983c22ed0edd
KimCookieYa <min49590@gmail.com>
2024-02-18 06:41:16 +0900

    Deploy: 프로덕션 서버 api 연결

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
+import axios from "axios";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+async function initAxios() {
+  axios.defaults.baseURL = import.meta.env.VITE_SERVER_API_URL;
+}
+
+initAxios().then(() =>
+  ReactDOM.createRoot(document.getElementById("root")!).render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  )
 );
