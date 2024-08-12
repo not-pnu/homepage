4a6818c8a2fd804f54a17d72c421ccb87aa8441b
KimCookieYa <min49590@gmail.com>
2024-08-13 01:01:34 +0900

    chore: 새 페이지로 리다이렉션되도록 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
-import { RouterProvider } from "react-router-dom";
+import {RouterProvider} from 'react-router-dom';
 import { QueryClientProvider } from "@tanstack/react-query";
 import "./App.css";
 
 import { router } from "./router";
 import { queryClient } from "./stores/QueryClient";
+import {useEffect} from 'react';
 
 function App() {
+    useEffect(() => {
+        window.location.href = "https://not-pnu.github.io/mailbadara/";
+    }, []);
+
   return (
     <QueryClientProvider client={queryClient}>
       <RouterProvider router={router} />
